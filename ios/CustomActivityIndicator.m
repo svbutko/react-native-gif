9e6395713c780f6a5f0f501e9e682522fbd69c19
svbutko <svbutko@hotmail.com>
2022-06-10 19:54:33 +0300

    Center the custom activity indicator

--- ios/CustomActivityIndicator.m
+++ ios/CustomActivityIndicator.m
@@ -1,4 +1,5 @@
 #import "CustomActivityIndicator.h"
+#import "CustomViewContainer.h"
 
 @interface CustomActivityIndicator ()
 @property (nonatomic, strong, readwrite, nonnull) UIView *indicatorView;
@@ -15,25 +16,7 @@
 }
 
 - (void)commonInit {
-    UIView *container = [[UIView alloc] init];
-
-    UIColor *backgroundColor = [UIColor colorWithRed:50.0 / 255.0 green:46.0 / 255.0 blue:77.0 / 255.0 alpha:1];
-    CGFloat circleColorValue = 239.0 / 255.0;
-    UIColor *circleColor = [UIColor colorWithRed:circleColorValue green:circleColorValue blue:circleColorValue alpha:1];
-
-    [container setAutoresizingMask:UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight];
-    [container setBackgroundColor:backgroundColor];
-
-    CAShapeLayer *circleLayer = [CAShapeLayer layer];
-
-    [circleLayer setPath:[[UIBezierPath bezierPathWithOvalInRect:CGRectMake(0, 0, 25, 25)] CGPath]];
-    [circleLayer setStrokeColor:[circleColor CGColor]];
-    [circleLayer setFillColor:[backgroundColor CGColor]];
-    [circleLayer setLineWidth:2.0];
-
-    [[container layer] addSublayer:circleLayer];
-
-    self.indicatorView = container;
+    self.indicatorView = [[CustomViewContainer alloc] init];
 }
 
 - (void)startAnimatingIndicator {
---
+++ ios/CustomViewContainer.h
@@ -0,0 +1,5 @@
+#import <UIKit/UIKit.h>
+
+@interface CustomViewContainer : UIView
+
+@end
